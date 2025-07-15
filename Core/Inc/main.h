980e7f49b7fbf60fe4880b0ec5a49f59ecd98dfa
Hiro <braydenannan@gmail.com>
2025-07-15 20:04:07 +1000

    Added UI functionality

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -15,13 +15,53 @@
 #include "task.h"
 #include "timers.h"
 #include "uart.h"
+#include "event_groups.h"
 
 #define PB0MASK		(3 << 0)
 #define PB0OUTPUT	(1 << 0)
 
+ /*
+   * Bit 0: Command - Enable LED
+   * Bit 1: Command - Disable LED
+   * Bit 2: Command - Blink LED
+   * Bit 3: Command - Invalid Input
+   * Bit 4: Command - Clear Screen
+   * Bit 5 - 9: Reserved
+   * Bit 10: Status - LED Enabled
+   * Bit 11: Status - LED Disabled
+   * Bit 12: Status - Blinking Enabled
+   * Bit 7 - 23: Reserved
+   */
+#define CTRL_GROUP_0		(1 <<  0)
+#define CTRL_GROUP_1		(1 <<  1)
+#define CTRL_GROUP_2		(1 <<  2)
+#define CTRL_GROUP_3		(1 <<  3)
+#define CTRL_GROUP_4		(1 <<  4)
+
+#define CTRL_GROUP_10		(1 << 10)
+#define CTRL_GROUP_11		(1 << 11)
+#define CTRL_GROUP_12		(1 << 12)
+
+#define CMD_ENABLE_LED		CTRL_GROUP_0
+#define CMD_DISABLE_LED		CTRL_GROUP_1
+#define CMD_BLINK_LED		CTRL_GROUP_2
+#define CMD_INVALID_INPUT	CTRL_GROUP_3
+#define CMD_CLEAR_SCREEN	CTRL_GROUP_4
+#define STATUS_LED_ENABLED	CTRL_GROUP_10
+#define STATUS_LED_DISABLED	CTRL_GROUP_11
+#define STATUS_BLINKING_ENABLED	CTRL_GROUP_12
+
 void vTaskPrint(void* pvParameters);
-void vTaskBlink(void* pvParameters);
-void vTaskEcho(void* pvParameters);
+void vTaskLED(void* pvParameters);
+void vTaskInput(void* pvParameters);
+
 void greenLedInit(void);
+void enableLed(void);
+void disableLed(void);
+void blinkLed(void);
+
+void printMainMenu(void);
+void clearScreen(void);
+void resetBlinkState(char* loopedState);
 
 #endif /* MAIN_H_ */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,17 +18,21 @@
 #include "main.h"
 
   TaskHandle_t xPrintTask;
-  TaskHandle_t xBlinkLed;
-  TaskHandle_t xEchoInput;
+  TaskHandle_t xControlLed;
+  TaskHandle_t xProcessInput;
+  EventGroupHandle_t xControlGroup;
 
 int main(void)
 {
   usart3_init();
   greenLedInit();
 
-  xTaskCreate(vTaskPrint, "vTaskPrint", 1024, NULL, 5, &xPrintTask);
-  xTaskCreate(vTaskBlink, "vTaskBlink", 1024, NULL, 4, &xBlinkLed);
-  xTaskCreate(vTaskEcho, "vTaskEcho", 1024, NULL, 3, &xEchoInput);
+
+  xControlGroup = xEventGroupCreate();
+
+  xTaskCreate(vTaskPrint, "vTaskPrint", 1024, NULL, 1, &xPrintTask);
+  xTaskCreate(vTaskLED, "vTaskLED", 1024, NULL, 2, &xControlLed);
+  xTaskCreate(vTaskInput, "vTaskInput", 1024, NULL, 1, &xProcessInput);
 
   vTaskStartScheduler();
 
@@ -36,29 +40,104 @@ int main(void)
 	for(;;);
 }
 
+
 void vTaskPrint(void* pvParameters){
-  int secondsPast = 0;
-  const TickType_t xDelay = 10000;
+  EventBits_t uxBits;
+  char looped = 0;
+  clearScreen();
+  printMainMenu();
   while(1){
-      vTaskDelay(xDelay);
-      secondsPast+=10;
-      printf("%d seconds has past\n\r", secondsPast);
+      uxBits = xEventGroupWaitBits(xControlGroup, 0x1C00, pdFALSE, pdFALSE, portMAX_DELAY);
+      if((uxBits & STATUS_LED_ENABLED) != 0){
+	  clearScreen();
+	  printMainMenu();
+	  printf("Led has been enabled!\n");
+	  xEventGroupClearBits(xControlGroup, STATUS_LED_ENABLED);
+	  resetBlinkState(&looped);
+      }else if((uxBits & STATUS_LED_DISABLED) != 0){
+	  clearScreen();
+	  printMainMenu();
+	  printf("Led has been disabled!\n");
+	  xEventGroupClearBits(xControlGroup, STATUS_LED_DISABLED);
+	  resetBlinkState(&looped);
+      }else if (((uxBits & STATUS_BLINKING_ENABLED) != 0) && looped == 0){
+	  clearScreen();
+	  printMainMenu();
+	  printf("Led is blinking!\n");
+	  looped = 1;
+      }else if((uxBits & CMD_CLEAR_SCREEN) != 0){
+	  clearScreen();
+	  printMainMenu();
+	  xEventGroupClearBits(xControlGroup, CMD_CLEAR_SCREEN);
+      }else if((uxBits & CMD_INVALID_INPUT) != 0){
+	  printf("Input was not an option please pick select 1, 2 or 3\n");
+	  xEventGroupClearBits(xControlGroup, CMD_INVALID_INPUT);
+      }
   }
 }
 
-void vTaskBlink(void* pvParameters){
-  const TickType_t delay = 100;
+/*
+ * TODO:	Find a alternative method for blinking that doesn't induce a UI freeze
+ * 		when switching from blinking state to enable/disable state. I.e. blink
+ * 		without vTaskDelay
+ *
+ */
+void vTaskLED(void* pvParameters){
+  EventBits_t uxBits;
+  const TickType_t xDelay = 1000/portTICK_PERIOD_MS;
   while(1){
-      vTaskDelay(delay);
-      GPIOB->ODR ^= PB0OUTPUT;
+    uxBits  = xEventGroupWaitBits(xControlGroup, 0x07, pdFALSE, pdFALSE,portMAX_DELAY);
+    if((uxBits & CMD_ENABLE_LED) != 0){
+	enableLed();
+	xEventGroupClearBits(xControlGroup, CMD_ENABLE_LED);
+	xEventGroupClearBits(xControlGroup, CMD_BLINK_LED);
+	xEventGroupSetBits(xControlGroup, STATUS_LED_ENABLED);
+    }else if((uxBits & CMD_DISABLE_LED) != 0){
+	disableLed();
+	xEventGroupClearBits(xControlGroup, CMD_DISABLE_LED);
+	xEventGroupClearBits(xControlGroup, CMD_BLINK_LED);
+	xEventGroupSetBits(xControlGroup, STATUS_LED_DISABLED);
+    }else if((uxBits & CMD_BLINK_LED) != 0){
+	blinkLed();
+	if ((uxBits & STATUS_BLINKING_ENABLED) == 0){
+	    xEventGroupSetBits(xControlGroup, STATUS_BLINKING_ENABLED);
+	}
+	vTaskDelay(xDelay);
+    }
   }
 }
 
-void vTaskEcho(void* pvParameters){
-  int input = 0;
+
+
+/*
+ * TODO:	Update UART driver to interrupt so that vTaskInput isn't always
+ * 		actively waiting for a char (essentially prevents reaching idle
+ * 		state and is always active)
+ */
+void vTaskInput(void* pvParameters){
+  uint32_t input = 0;
   while(1){
       input = usart3_rx_blocking();
-      printf("Echo: %c\n\r", input);
+      switch(input){
+	case 49:
+	  xEventGroupSetBits(xControlGroup, CMD_ENABLE_LED);
+	  break;
+	case 50:
+	  xEventGroupSetBits(xControlGroup, CMD_DISABLE_LED);
+	  break;
+	case 51:
+	  xEventGroupSetBits(xControlGroup, CMD_BLINK_LED);
+	  break;
+	case 67:
+	  xEventGroupSetBits(xControlGroup, CMD_CLEAR_SCREEN);
+	  break;
+	case 99:
+	  xEventGroupSetBits(xControlGroup, CMD_CLEAR_SCREEN);
+	  break;
+	default:
+	  xEventGroupSetBits(xControlGroup, CMD_INVALID_INPUT);
+	  break;
+      }
   }
 }
 
@@ -66,6 +145,36 @@ void greenLedInit(void){
   RCC->AHB4ENR |= RCC_AHB4ENR_GPIOBEN;
   GPIOB->MODER &= ~(PB0MASK);
   GPIOB->MODER |= PB0OUTPUT;
+}
+
+void enableLed(void){
+  GPIOB->ODR |= PB0OUTPUT;
+}
+
+void disableLed(void){
+  GPIOB->ODR &= ~(PB0OUTPUT);
+}
+
+void blinkLed(void){
+  GPIOB->ODR ^= PB0OUTPUT;
+}
+
+void printMainMenu(void){
+  printf("Welcome to the LED CLI! Please select an option below:\n"
+      "1.Enable LED\n"
+      "2.Disable LED\n"
+      "3.Blink LED\n\n"
+      "Press C to clear the screen!\n");
+}
+
+void clearScreen(void){
+  // ANSI and VT102 escape code "Esc[2J" clears terminal screens once received.
+  printf("%c%c%c%c\n",0x1B,0x5B,0x32,0x4A);
+}
+
+void resetBlinkState(char* loopedState){
+  xEventGroupClearBits(xControlGroup, STATUS_BLINKING_ENABLED);
+  *loopedState = 0;
 }
 
 
